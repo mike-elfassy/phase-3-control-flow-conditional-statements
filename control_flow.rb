2fcbe20022c2197763199a2156e3145415698b02
Michael Elfassy <mike.elfassy@gmail.com>
2023-06-03 13:46:34 -0400

    commit tree 373e15bbd69a7d76275ba6c633806e96c1b314a0

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,41 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  if username.downcase == "admin" && password == '12345'
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  case temperature
+    when -Float::INFINITY...40 then "It's brisk out there!"
+    when 41...65 then "It's a little chilly out there!"
+    when 66...85 then "It's perfect out there!"
+    else "It's too dang hot out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  output = ''
+  if num % 3 == 0 then output = 'Fizz' end
+  if num % 5 == 0 then output = output + 'Buzz' end
+  if output == '' then output = num end
+  output
 end
 
+# binding.pry
+
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when '+' then num1 + num2
+  when '-' then num1 - num2
+  when '*' then num1 * num2
+  when '/' then num1 / num2
+  else
+      puts "Invalid operation!"
+      nil
+  end
 end
 
